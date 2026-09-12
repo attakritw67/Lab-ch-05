8db6089950969e84bfd32d6cf8bebf8aebd66386
attakritw67 <attakritw67@nu.ac.th>
2026-09-13 00:06:54 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login_form_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,30 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyhomePage(),
-    );
-  }
-}
-
-class MyhomePage extends StatelessWidget {
-  const MyhomePage({super.key});
-
-@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-    appBar: AppBar(title: const Text('ปฏิบัติการที่ 5 - Layouts'),
-    backgroundColor: Colors.indigo,
-    ),
-    body: const Padding(padding: EdgeInsets.all(16.0),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Icon(Icons.home,size:40.0,color: Colors.blue),
-        Icon(Icons.search,size:40.0,color: Colors.green),
-        Icon(Icons.person,size:40.0,color: Colors.orange),
-      ],
-    ),
-      )
+      home: LoginFormScreen(),
     );
   }
 }
